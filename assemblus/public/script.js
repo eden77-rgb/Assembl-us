9ece0ad4515440a62e244559699fe59b27e380ec
Eden Vandewatyne <eden.vandewatyne@estiam.com>
2025-05-30 13:13:12 +0200

    Ajout d'un effet par caractère

--- assemblus/public/script.js
+++ assemblus/public/script.js
@@ -1,13 +1,39 @@
 const lines = document.querySelectorAll('.line');
+const scrollText = document.getElementById('scrollText');
+
+const alreadyTyped = new Set()
+
+function typeLine(line, text) {
+    let index = 0
+
+    function typeChar() {
+        if (index < text.length) {
+            line.textContent += text[index]
+            index += 1
+
+            setTimeout(typeChar, 30)
+        }
+    }
+
+    typeChar()
+}
 
 window.addEventListener('scroll', () => {
     const scrollY = window.scrollY;
-    
+
+    if (scrollY > 50) {
+        scrollText.classList.add('hidden');
+    }
+
     lines.forEach((line, i) => {
         const trigger = 200 + i * 200;
-        
-        if (scrollY > trigger) {
+        const text_line = line.dataset.text
+
+        if (scrollY > trigger && !alreadyTyped.has(line)) {
+
             line.classList.add('visible');
+            typeLine(line, text_line)
+            alreadyTyped.add(line)
         }
     });
 });
