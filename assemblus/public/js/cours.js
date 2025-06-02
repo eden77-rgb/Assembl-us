435d65d9d39ba21dfc6e3906c93376bd5cdb3f55
Eden Vandewatyne <eden.vandewatyne@estiam.com>
2025-06-02 12:02:52 +0200

    Refactor de cours.js

--- assemblus/public/js/cours.js
+++ assemblus/public/js/cours.js
@@ -1,10 +1,3 @@
-const chap1 = document.getElementById("1")
-const chap2 = document.getElementById("2")
-const chap3 = document.getElementById("3")
-const chap4 = document.getElementById("4")
-const chap5 = document.getElementById("5")
-const chap6 = document.getElementById("6")
-
 const h1 = document.querySelector("h1")
 const text = h1.textContent
 h1.textContent = ""
@@ -23,38 +16,12 @@ function typeChar() {
 
 typeChar()
 
-chap1.addEventListener("click", () => {
-    const urlPath = location.pathname
-    history.pushState(null, "", `${urlPath}/chapitre1`)
-    location.reload()
-})
+for (let i = 1; i <= 6; i++) {
+    const chapId = document.getElementById(i.toString())
 
-chap2.addEventListener("click", () => {
-    const urlPath = location.pathname
-    history.pushState(null, "", `${urlPath}/chapitre2`)
-    location.reload()
-})
-
-chap3.addEventListener("click", () => {
-    const urlPath = location.pathname
-    history.pushState(null, "", `${urlPath}/chapitre3`)
-    location.reload()
-})
-
-chap4.addEventListener("click", () => {
-    const urlPath = location.pathname
-    history.pushState(null, "", `${urlPath}/chapitre4`)
-    location.reload()
-})
-
-chap5.addEventListener("click", () => {
-    const urlPath = location.pathname
-    history.pushState(null, "", `${urlPath}/chapitre5`)
-    location.reload()
-})
-
-chap6.addEventListener("click", () => {
-    const urlPath = location.pathname
-    history.pushState(null, "", `${urlPath}/chapitre6`)
-    location.reload()
-})
+    chapId.addEventListener("click", () => {
+        const urlPath = location.pathname
+        history.pushState(null, "", `${urlPath}/chapitre${i.toString()}`)
+        location.reload()
+    })
+}
